fbda9dc5d9c2ceb3000b5bed8b4a6fd5f286d75f
Majid Ali Khan <majidalykhan@gmail.com>
2023-01-03 01:18:05 +0500

    Cloudinary configuration

---
+++ Backend/config/cloudinary.js
@@ -0,0 +1,21 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+//Configure cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+//Instance of cloudinary storage
+const storage = new CloudinaryStorage({
+  cloudinary,
+  allowedFormats: ["jpg", "png"],
+  params: {
+    folder: "blog-api",
+    transformation: [{ width: 500, height: 500, crop: "Limit" }],
+  },
+});
+
+module.exports = storage;
