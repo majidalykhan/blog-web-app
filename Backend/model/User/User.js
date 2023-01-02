d72e135298c17e033e62f13e9fdbe2039fe1aea7
Majid Ali Khan <majidalykhan@gmail.com>
2023-01-03 01:03:33 +0500

    User model modified

--- Backend/model/User/User.js
+++ Backend/model/User/User.js
@@ -23,10 +23,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is required"],
     },
-    postCount: {
-      type: Number,
-      default: 0,
-    },
+
     isBlocked: {
       type: Boolean,
       default: false,
@@ -39,7 +36,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       enum: ["Admin", "Guest", "Editor"],
     },
-    viewedBy: [
+    viewers: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
@@ -57,14 +54,30 @@ const userSchema = new mongoose.Schema(
         ref: "User",
       },
     ],
-    active: {
-      type: Boolean,
-      default: true,
-    },
     posts: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Post",
+      },
+    ],
+    blocked: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    plan: [
+      {
+        type: String,
+        enum: ["Free", "Premium", "Pro"],
+        default: "Free",
+      },
+    ],
+    userAward: [
+      {
+        type: String,
+        enum: ["Bronze", "Silver", "Gold"],
+        default: "Bronze",
       },
     ],
   },
