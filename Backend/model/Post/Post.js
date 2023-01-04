ad736e856e024ef5baaddc7adca7cd36f9544ae4
Majid Ali Khan <majidalykhan@gmail.com>
2023-01-04 17:55:47 +0500

    Number of post views count

--- Backend/model/Post/Post.js
+++ Backend/model/Post/Post.js
@@ -2,51 +2,65 @@ const mongoose = require("mongoose");
 
 //Create schema
 
-const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Post Title is required"],
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: [true, "Post Description is required"],
-    trim: true,
-  },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
-    required: [true, "Post Category is required"],
-  },
-  numViews: [
-    {
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Post Title is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Post Description is required"],
+      trim: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Post Category is required"],
+    },
+    numViews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: [true, "Please Author is required"],
     },
-  ],
-  likes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+    photo: {
+      type: String,
+      // required: [true, "Post Image is required"],
     },
-  ],
-  dislikes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "Please Author is required"],
   },
-  photo: {
-    type: String,
-    // required: [true, "Post Image is required"],
-  },
-},{
-    timestamps:true
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+);
+
+//Hook
+postSchema.pre(/^find/, function (next) {
+  //Add views count as virtual field
+  postSchema.virtual("viewsCount").get(function () {
+    const post = this;
+    return post.numViews.length;
+  });
+  next();
 });
 
 //Compile the post model
